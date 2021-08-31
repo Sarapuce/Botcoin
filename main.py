7d19f41baf319c6b147f8e5a43819a1028a543ac
Herve Alexandre <alexandre.herve@supelec.fr>
2021-08-31 20:07:11 +0200

    All variables ok

--- main.py
+++ main.py
@@ -6,22 +6,26 @@ import json
 import time
 import curses
 
+# Use of the wrapper function to get thing easier
 def init_curses():
     curses.wrapper(main)
 
+# Definition of color here
 def define_color():
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
 
+# main function each second it will update the interface
 def main(stdscr):
     stdscr.nodelay(1)
     define_color()
     price = 0
     order_list = []
+    budget = 10000
 
-    place_order(order_list, 'A', 30000, 60000, 35000, 0.01)
-    place_order(order_list, 'V', 90000, 60000, 70000, 0.01)
+    budget -= place_order(order_list, 'A', 30000, 60000, 35000, 0.01)
+    budget -= place_order(order_list, 'V', 90000, 60000, 70000, 0.01)
 
     try:
         print_header(stdscr)
@@ -33,6 +37,7 @@ def main(stdscr):
             update_orders(order_list, price)
             print_orders(stdscr, order_list)
             print_ema(stdscr)
+            print_budget(stdscr, budget)
             stdscr.refresh()
             if stdscr.getch() == ord('q'):
                 break
@@ -40,6 +45,7 @@ def main(stdscr):
     except KeyboardInterrupt:
         curses.endwin()
 
+# Graphical functions
 def print_header(stdscr):
     stdscr.addstr('+--------------------------+\n', curses.color_pair(3))
     stdscr.addstr('|          Botcoin         |\n', curses.color_pair(3))
@@ -69,6 +75,7 @@ def print_orders(stdscr, order_list):
         stdscr.addstr(4*i + 0, 90, 'Value : ' + str(order['value']))
         stdscr.addstr(4*i + 1, 50, 'SL : ' + str(order['SL']))
         stdscr.addstr(4*i + 1, 70, 'TP : ' + str(order['TP']))
+        stdscr.addstr(4*i + 1, 90, 'Current value : {}'.format(order['value']))
         stdscr.addstr(4*i + 2, 50, 'Profit : {:.2f}'.format(order['profit']), c)
         stdscr.addstr(4*i + 2, 70, '{:.2f}%'.format(order['percent']), c)
 
@@ -78,9 +85,26 @@ def print_ema(stdscr):
     stdscr.addstr(8, 0, "EMA 5  : {:.2f}".format(ema5))
     stdscr.addstr(9, 0, "EMA 20 : {:.2f}".format(ema20))
 
-def place_order(order_list, type_, SL, TP, price, qty):
-    order_list.append({'type' : type_, 'price' : price, 'quantity' : qty, 'value' : qty*price, 'SL' : SL, 'TP' : TP, 'profit' : 0, 'percent' : 0})
+def print_budget(stdscr, budget):
+    stdscr.addstr(11, 0, "Budget : {:.2f}".format(budget))
 
+# Add an order to the list. The format is not devinitive
+def place_order(order_list, type_, SL, TP, price, qty):
+    order_list.append({'type' : type_, 'price' : price, 'quantity' : qty, 'value' : qty*price, 'current_value' : qty*price, 'SL' : SL, 'TP' : TP, 'profit' : 0, 'percent' : 0})
+    budget = qty*price
+    return budget
+
+def sell_order(order_list, id_):
+    current_price = get_price()
+    update_orders(order_list, current_price)
+    order = order_list[id_]
+    order_list = order_list[:i] + order_list[i+1:]
+    budget = order['value'] + order['profit']
+    return budget
+
+
+
+# Calculate profit from each trade
 def update_orders(order_list, current_price):
     for order in order_list:
         price = order['price']
@@ -91,11 +115,13 @@ def update_orders(order_list, current_price):
             order['profit']  = (price - current_price) * order['quantity']
             order['percent'] =  ((price / current_price) - 1) * 100   
 
+# Get the price of BTC thought binance API
 def get_price(symbol = 'BTCUSDT'):
     r = requests.get('https://api.binance.com/api/v3/ticker/price?symbol=' + symbol)
     price = json.loads(r.text)['price']
     return float(price)
 
+# Calculate EMA from a serie and a period (for trading view)
 def calculate_ema(prices, days, smoothing=2):
     smoothing = 2
     ema = [sum(prices[:days]) / days]
@@ -103,6 +129,7 @@ def calculate_ema(prices, days, smoothing=2):
         ema.append((price * (smoothing / (1 + days))) + ema[-1] * (1 - (smoothing / (1 + days))))
     return ema
 
+# Calculate ema with closed candle from binance API
 def get_ema(period, symbol = 'BTCUSDT'):
     url = 'https://api.binance.com/api/v3/klines?symbol='+ symbol +'&interval='+'1m'
     data = requests.get(url).json()
