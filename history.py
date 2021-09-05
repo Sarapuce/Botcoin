148afaf6ba012bdd892679c6c5f30b5a0c8f9e97
Herve Alexandre <alexandre.herve@supelec.fr>
2021-09-05 20:00:50 +0200

    Added extract_history

---
+++ history.py
@@ -0,0 +1,17 @@
+import requests
+import pandas as pd
+
+def extract_history(symbol, interval, start, end):
+    df = pd.DataFrame()
+    while end > start:
+        url = 'https://api.binance.com/api/v3/klines?symbol=' + symbol +'&interval='+ interval +'&limit=1000&endTime=' + str(end)+ '000'
+        print(url)
+        data = requests.get(url).json()
+        df = pd.concat([df, pd.DataFrame(data)])
+        
+        interval_millisec = data[1][0] - data[0][0]
+        end = (data[0][0] - interval_millisec) / 1000
+        end = int(end)
+        
+    df = df[df[0] >= start * 1000]
+    return df
