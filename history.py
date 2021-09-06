85726eb00ce1b7dd1575b434a5b25277d7da3b50
Herve Alexandre <alexandre.herve@supelec.fr>
2021-09-06 11:24:06 +0200

    Fix order while concat df

--- history.py
+++ history.py
@@ -7,7 +7,7 @@ def extract_history(symbol, interval, start, end):
         url = 'https://api.binance.com/api/v3/klines?symbol=' + symbol +'&interval='+ interval +'&limit=1000&endTime=' + str(end)+ '000'
         print(url)
         data = requests.get(url).json()
-        df = pd.concat([df, pd.DataFrame(data)])
+        df = pd.concat([pd.DataFrame(data), df])
         
         interval_millisec = data[1][0] - data[0][0]
         end = (data[0][0] - interval_millisec) / 1000
